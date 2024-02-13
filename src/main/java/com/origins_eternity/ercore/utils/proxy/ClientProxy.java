998c9129c092b4da602734772348a649c6d21b20
Origins_Eternity <origins.eternity@outlook.com>
2024-02-13 18:18:23 +0800

    Update ClientProxy.java

--- src/main/java/com/origins_eternity/ercore/utils/proxy/ClientProxy.java
+++ src/main/java/com/origins_eternity/ercore/utils/proxy/ClientProxy.java
@@ -58,11 +58,8 @@ public class ClientProxy extends CommonProxy {
         for (String resourcepack : Configuration.resourcepacks) {
             File pack = new File(gameDir() + "\\resourcepacks\\" + resourcepack);
             File backup = new File(gameDir() + "\\backup\\" + resourcepack);
-            File origin = new File(gameDir() + "\\mods\\" + resourcepack + ".disabled");
-            File resource = new File(gameDir() +"\\resourcepacks\\" + resourcepack + ".disabled");
-            if ((resource.exists()) && (!pack.exists())) {
-                resource.renameTo(pack);
-            } else if ((origin.exists()) && (!pack.exists())) {
+            File origin = new File(gameDir() + "\\mods\\" + resourcepack);
+            if ((origin.exists()) && (!pack.exists())) {
                 origin.renameTo(pack);
             } else if ((backup.exists()) && (!pack.exists())) {
                 backup.renameTo(pack);
