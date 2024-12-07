db2f15f6d405b974fea7e0f43590ea1a024d125a
Origins_Eternity <origins.eternity@outlook.com>
2024-12-07 16:26:10 +0800

    Update Items.java

--- src/main/java/com/origins_eternity/ercore/campat/firstaid/Items.java
+++ src/main/java/com/origins_eternity/ercore/campat/firstaid/Items.java
@@ -11,7 +11,7 @@ import java.util.function.Function;
 import static com.origins_eternity.ercore.content.tab.CreativeTab.ERCORE;
 
 public class Items extends ItemHealing {
-    public static final ItemHealing Herbs = new Items("Herbs", stack -> new PartHealer(23 * 20, 2, stack), stack -> 2000);
+    public static final ItemHealing Herbs = new Items("Herbs", stack -> new PartHealer(15 * 20, 3, stack), stack -> 2000);
 
     public Items(String name, Function<ItemStack, AbstractPartHealer> healerFunction, Function<ItemStack, Integer> time) {
         super(healerFunction, time);
